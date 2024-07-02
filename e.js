9873098dd816affbb5625bec130437de9d968343
Heitor <4553425989@estudante.sed.sc.gov.br>
2024-07-01 21:42:39 -0300

    criada selecao de exercicios

---
+++ e.js
@@ -0,0 +1,48 @@
+let nome = 'banana'
+
+console.log(nome) 
+
+let  a=5  , b= 10, soma=a+b 
+console.log (soma)
+
+nome = 'josefa',ultimonome = 'silva', junta= nome+ ultimonome  
+console.log(junta)
+let pessoa = {
+    nome: 'josefa',
+    idade: 15,
+    endereco: 20 ,
+    telefone: 47985595187,
+}
+
+let numeros =[1,2,3,4,5]
+numeros.push(6)
+console.log(numeros)
+
+let cores = ['vermelho','verde','azul']
+
+for (let i=0; i<3; i++){
+    console.log(cores[i])  
+}
+  let idade=20
+  if (idade<10){
+    console.log ('crianca') 
+  } else if (idade<18){
+    console.log ('adolecencia')
+  } else if (idade<60) {
+   console.log ('adulto') 
+  }else{ 
+   console.log ('idoso')  
+  }
+    
+  
+
+
+
+
+
+
+
+
+
+
+
